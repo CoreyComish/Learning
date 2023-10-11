2985c56a26158a253c253961f5e9fa1ab711b8bc
Corey Comish <coreycomish@gmail.com>
2023-10-11 16:56:22 -0500

    Added first folder and file with todo's

---
+++ BigO/main.py
@@ -0,0 +1,21 @@
+# Purpose: Learn and provide examples of Big O
+
+# TODO: What is Big O?
+# TODO: What is log?
+# TODO: Why does this matter?
+# TODO: Does this apply to data structures?
+
+# TODO: Implement O(1) Algorithm
+
+# TODO: Implement O(log n) Algorithm
+
+# TODO: Implement O(n log n) Algorithm
+
+# TODO: Implement O(n^2) Algorithm
+
+# TODO: Implement O(2^n) Algorithm
+
+# TODO: Implement O(n!) Algorithm
+
+def main():
+    pass
