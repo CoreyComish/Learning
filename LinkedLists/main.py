4ef70c587fb427edd3b525b28834f6d579bca74b
Corey Comish <coreycomish@gmail.com>
2023-10-16 15:30:50 -0500

    Setup for next section, LinkedLists

---
+++ LinkedLists/main.py
@@ -0,0 +1,15 @@
+# Purpose: Learn and provide examples of Linked Lists
+
+# What is a Linked List?
+
+# What are the different types of Linked Lists?
+
+# Linked List opearations and size
+
+# Linked list problem / algorithms
+
+def main():
+    pass
+
+if __name__=="__main__": 
+    main() 
