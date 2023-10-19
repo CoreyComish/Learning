a0ab4b9a930c1c691efca1e4b0a308dc29c9eb25
Corey Comish <coreycomish@gmail.com>
2023-10-18 19:37:17 -0500

    Queue start

---
+++ Data Structures/Queue.py
@@ -0,0 +1,16 @@
+# Purpose: Learn and provide examples of a Queue
+
+# What is a Queue?
+
+# What is a Queue useful for?
+
+# Queue operations and size
+
+# Queue implementation
+
+def main():
+    pass
+
+if __name__=="__main__": 
+    main() 
+
