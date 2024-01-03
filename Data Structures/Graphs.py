55a69284f7380b3da76b2236c7d56b46f7a20eb0
Corey Comish <coreycomish@gmail.com>
2024-01-03 15:46:06 -0600

    Graph setup

--- Data Structures/Graphs.py
+++ Data Structures/Graphs.py
@@ -1,9 +1,21 @@
 # Purpose: Learn and provide examples of Graphs
 
 # What is a Graph?
+"""
+"""
 
 # What is a Graph useful for?
+"""
+"""
 
 # Graph operations and space
+"""
+"""
 
 # Graph implementation
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
